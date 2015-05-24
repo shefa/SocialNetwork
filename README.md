3468958814a88b58a453bc519bf12d0dee9832ef
venom1724 <venom1724@gmail.com>
2015-05-24 12:17:51 +0300

    Placed all files within app/ and added web server

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# SocialNetwork
-AngularJS practical project for softuni
+## "Social-Network"
+### "Social Network" is practical project assignment from "JS Frameworks with AngularJS" course at Software University.
---
+++ app/js/controllers/EditProfileController.js
@@ -0,0 +1,36 @@
+'use strict';
+
+socialNetworkApp.controller('EditProfileController',
+    ['$scope', '$location', '$timeout', 'userData', 'credentials', 'toaster', function ($scope, $location, $timeout, userData, credentials, toaster){
+        $scope.editUser = credentials.getLoggedUser();
+        $scope.editProfile = editProfile;
+        $scope.formatProfileImgToBase64 = formatProfileImgToBase64;
+        $scope.formatCoverImgToBase64 = formatCoverImgToBase64;
+
+        function editProfile(user, editProfileForm) {
+            userData.edit(user)
+                .$promise
+                .then(function (data) {
+                    $scope.editProfileForm.$setPristine();
+                    toaster.pop('success', 'Edit successful!', data.message, 2000);
+                    redirectToHome(2000);
+                }, function (error) {
+                    toaster.pop('error', 'Edit profile error!', error.data.message, 2000);
+                })
+        }
+
+        function formatProfileImgToBase64() {
+            $scope.editUser.profileImageData = 'data:image/jpg;base64,' + $scope.editUser.profileImageData.base64;
+        }
+
+        function formatCoverImgToBase64() {
+            $scope.editUser.coverImageData = 'data:image/jpg;base64,' + $scope.editUser.coverImageData.base64;
+        }
+
+        function redirectToHome(time) {
+            $timeout(function () {
+                $location.path('/');
+            }, time);
+        }
+    }
+    ]);
